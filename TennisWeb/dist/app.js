6ede07c0f55cd0eb9c0ce38761d435f62737e599
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2022-05-28 12:16:04 +0200

    Bookings and People

--- TennisWeb/dist/app.js
+++ TennisWeb/dist/app.js
@@ -15,7 +15,86 @@ var BookingDto = /** @class */ (function () {
 exports.BookingDto = BookingDto;
 },{}],2:[function(require,module,exports){
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var baseUrl = "http://localhost:5000/";
+function fillPersonTable() {
+    $("#tblPeople").empty()
+        .append("<tr>\n" +
+        "            <th>Nachname</th>\n" +
+        "            <th>Vorname</th>\n" +
+        "            <th>Alter</th>\n" +
+        "            <th>#Reservierungen</th>\n" +
+        "        </tr>");
+    $.getJSON("".concat(baseUrl, "api/Person"))
+        .then(function (people) {
+        people.forEach(function (x) {
+            var row = $("<tr>");
+            row.append($("<td>").text(x.lastName));
+            row.append($("<td>").text(x.firstName));
+            row.append($("<td>").text(x.age));
+            row.append($("<td>").text(x.bookingCount));
+            $("#tblPeople").append(row);
+        });
+    });
+}
+function fillBookings() {
+    var bookingHeader = "<tr>\n" +
+        "            <th></th>\n" +
+        "            <th>Mo</th>\n" +
+        "            <th>Di</th>\n" +
+        "            <th>Mi</th>\n" +
+        "            <th>Do</th>\n" +
+        "            <th>Fr</th>\n" +
+        "            <th>Sa</th>\n" +
+        "            <th>So</th>\n" +
+        "   </tr>";
+    $("#tblBookings").empty()
+        .append(bookingHeader);
+    for (var i = 10; i <= 22; i++) {
+        var row = $("<tr>");
+        row.append($("<td>").text(i));
+        for (var j = 0; j < 7; j++) {
+            row.append($("<td>").text("").attr("id", "".concat(i, "-").concat(j)));
+        }
+        $("#tblBookings").append(row);
+    }
+    var week = $('#week').text();
+    console.log(week);
+    $.getJSON("".concat(baseUrl, "api/Booking"))
+        .then(function (bookings) {
+        bookings.forEach(function (x) {
+            if (x.week == week) {
+                console.log(x.id);
+                $("#".concat(x.hour, "-").concat(x.dayOfWeek)).text("hi");
+            }
+        });
+    });
+}
 $(function () {
+    $('#people').hide();
+    $('#bookings').hide();
+    $('#decreaseWeek').on('click', function () {
+        var week = $('#week').text();
+        if (Number(week) > 1) {
+            $('#week').text(Number(week) - 1);
+            fillBookings();
+        }
+    });
+    $('#increaseWeek').on('click', function () {
+        var week = $('#week').text();
+        $('#week').text(Number(week) + 1);
+        fillBookings();
+    });
+    $('#showPeople').on("click", function () {
+        $('#people').show();
+        $('#bookings').hide();
+        fillPersonTable();
+    });
+    $('#showBookings').on("click", function () {
+        $('#people').hide();
+        $('#bookings').show();
+        fillBookings();
+    });
 });
 },{}],3:[function(require,module,exports){
 "use strict";
--- TennisWeb/src/index.ts
+++ TennisWeb/src/index.ts
@@ -1,3 +1,89 @@
-﻿$(() => {
+﻿import {PersonDto} from "./person";
+import {BookingDto} from "./booking";
+
+const baseUrl = "http://localhost:5000/";
+
+function fillPersonTable() {
+    $("#tblPeople").empty()
+        .append("<tr>\n" +
+        "            <th>Nachname</th>\n" +
+        "            <th>Vorname</th>\n" +
+        "            <th>Alter</th>\n" +
+        "            <th>#Reservierungen</th>\n" +
+        "        </tr>")
+    $.getJSON(`${baseUrl}api/Person`)
+        .then((people: PersonDto[]) => {
+            people.forEach(x => {
+                const row = $("<tr>");
+                row.append($("<td>").text(x.lastName));
+                row.append($("<td>").text(x.firstName));
+                row.append($("<td>").text(x.age));
+                row.append($("<td>").text(x.bookingCount));
+                $("#tblPeople").append(row);
+            })
+        })
+}
+
+function fillBookings() {
+    const bookingHeader = "<tr>\n" +
+        "            <th></th>\n" +
+        "            <th>Mo</th>\n" +
+        "            <th>Di</th>\n" +
+        "            <th>Mi</th>\n" +
+        "            <th>Do</th>\n" +
+        "            <th>Fr</th>\n" +
+        "            <th>Sa</th>\n" +
+        "            <th>So</th>\n" +
+        "   </tr>"
     
+    $("#tblBookings").empty()
+        .append(bookingHeader)
+    
+    for (let i = 10; i <= 22; i++) {
+        const row = $("<tr>");
+        row.append($("<td>").text(i));
+        for (let j = 0; j < 7; j++) {
+            row.append($("<td>").text("").attr("id", `${i}-${j}`));
+        }
+        $("#tblBookings").append(row);
+    }
+    const week = $('#week').text()
+    console.log(week)
+    $.getJSON(`${baseUrl}api/Booking`)
+        .then((bookings: BookingDto[]) => {
+            bookings.forEach(x => {
+                if (x.week == week){
+                    console.log(x.id)
+                    $(`#${x.hour}-${x.dayOfWeek}`).text("hi");
+                }
+            })
+        })
+}
+
+$(() => {
+    $('#people').hide();
+    $('#bookings').hide();
+    $('#decreaseWeek').on('click', () => {
+        const week = $('#week').text();
+        if (Number(week) > 1) {
+            $('#week').text(Number(week) - 1);
+            fillBookings();
+        }
+    });
+    $('#increaseWeek').on('click', () => {
+        const week = $('#week').text();
+        $('#week').text(Number(week) + 1);
+        fillBookings();
+    });
+    
+    $('#showPeople').on("click", function () {
+        $('#people').show();
+        $('#bookings').hide();
+        fillPersonTable();
+    })
+    $('#showBookings').on("click", function () {
+        $('#people').hide();
+        $('#bookings').show();
+        fillBookings();
+    })
 })
